9ae7ba48e16f2e37b20159f8d5e57db274dfb2ca
Jaden <jadenseangmany@gmail.com>
2025-02-23 14:29:24 -0800

    Update jest.config.js
    for es modules parsing failure:
    - this forces Jest to transpile react-native-calendars, react-navigation, and react-native.

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,8 @@ module.exports = {
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|webp|svg|png)$': '<rootDir>/__mocks__/fileMock.js',
   },
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|@react-native|@react-navigation|react-native-calendars)/)',
+  ], 
   preset: 'react-native',
 };
