c5bb1f0caecc8f2bd5dcc51a4eac40d287324992
Jaden <jadenseangmany@gmail.com>
2025-02-23 21:35:30 -0800

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,12 @@
 module.exports = {
+  preset: 'react-native',
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|webp|svg|png)$': '<rootDir>/__mocks__/fileMock.js',
   },
   transformIgnorePatterns: [
     'node_modules/(?!(react-native|@react-native|@react-navigation|react-native-calendars)/)',
-  ], 
-  preset: 'react-native', 'ts-jest',
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
-  },
+  ],
 };
