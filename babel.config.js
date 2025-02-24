be609935696654d7cb79d65a42ddafade17292d4
Jaden <jadenseangmany@gmail.com>
2025-02-23 21:30:26 -0800

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   presets: [
     'module:@react-native/babel-preset',
-    ['module:metro-react-native-babel-preset'],
+    'module:metro-react-native-babel-preset',
     '@babel/preset-typescript',],
 };
